31d6d5c4093bfa9c8878db54b03d0b8d266570d9
Brendan O'Rourke <brendan@optimizely.com>
2016-08-29 12:03:40 -0700

    Remove superfluous code and make style more consistent

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 'use strict';
 
 require('dotenv').config();
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
-var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+var fs = require('fs'),
+    http = require('http'),
+    https = require('https');
+
+/**
+ * Credentials for MailGun service. Must be stored locally.
+ * Contact andreas@optimizely.com with questions.
+ */
+var privateKey  = fs.readFileSync('ssl/server.key', 'utf8'),
+    certificate = fs.readFileSync('ssl/server.crt', 'utf8'),
+    credentials = {key: privateKey, cert: certificate};
 
 const express = require('express'),
       mg = require('mailgun-js'),
-      AWS = require('aws-sdk'),
       optly = require('optimizely-server-sdk');
 
 const api_key = process.env.MAILGUN_API_KEY,
       domain = process.env.MAILGUN_DOMAIN,
-      AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY,
+      mailer = mg({apiKey: api_key, domain: domain}),
       sender = 'Optimizely <me@' + domain +'>';
 
-//console.log(datafile);
-
 //let optimizely = optly.createInstance({datafile:datafile});
-
-//AWS.config(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY);
-//let dynamodb = new AWS.DynamoDB();
-
-const mailer = mg({apiKey: api_key, domain: domain});
-const app = express();
 
 const images = {
   "shirts": "http://cdn.optimizely.com/img/3546160213/e4aa2777a11542a782be2697e4ca5426.jpg",
   "jeans": "http://cdn.optimizely.com/img/3546160213/e4aa2777a11542a782be2697e4ca5426.jpg"
-
 }
+
+const app = express();
 
 app.get('/' , (req,res) => {
   res.send('Hello World');
