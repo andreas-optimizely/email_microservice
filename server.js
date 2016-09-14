6050c63354b08b2381a3c9049cefa5d276e02872
andreas-optimizely <andreas@optimizely.com>
2016-09-13 22:33:55 -0700

    update for SST

--- server.js
+++ server.js
@@ -4,29 +4,51 @@ require('dotenv').config();
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-//var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
-//var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
-//var credentials = {key: privateKey, cert: certificate};
 
 const express = require('express'),
       mg = require('mailgun-js'),
       AWS = require('aws-sdk'),
-      optly = require('optimizely-server-sdk');
+      optly = require('optimizely-server-sdk'),
+      request = require('request');
 
 const api_key = process.env.MAILGUN_API_KEY,
       domain = process.env.MAILGUN_DOMAIN,
       AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+      AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY,
+      DATAFILE_URL = 'https://cdn.optimizely.com/json/7452801066.json';
 
-//console.log(datafile);
+var defaultErrorHandler = require('optimizely-server-sdk/lib/plugins/error_handler');
+var defaultLogger = require('optimizely-server-sdk/lib/plugins/logger');
 
-//let optimizely = optly.createInstance({datafile:datafile});
+function getDatafile(URL){
+  return new Promise((resolve, reject) => {
+    request({url:DATAFILE_URL, json:true}, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        resolve(body);
+      }
+      else {
+        reject('Rejected');
+      }
+    });
+  })
+}
 
-//AWS.config(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY);
-//let dynamodb = new AWS.DynamoDB();
+/**
+  Initialize Optimizely on Server start
+*/
+let optimizely;
+getDatafile().then((data) => {
+  console.log('OPTIMIZELY DEFINTED ', data);
+  data.dimensions = [];
+  return optimizely = optly.createInstance({
+            datafile: data,
+            errorHandler: defaultErrorHandler,
+            logger: defaultLogger.createLogger(),
+          });
+});
 
-const mailer = mg({apiKey: api_key, domain: domain});
-const app = express();
+const mailer = mg({apiKey: api_key, domain: domain}),
+      app = express();
 
 app.get('/' , (req,res) => {
   res.send('Hello World');
@@ -50,7 +72,7 @@ app.get('/send-ww-welcome', (req,res) => {
 });
 
 app.get('/send-best-buy', (req,res) => {
-  let email = req.query.email;
+  let email = decodeURIComponent(req.query.email);
 
   let sender = 'Best Buy <me@' + domain +'>';
   let data = {
@@ -68,21 +90,46 @@ app.get('/send-best-buy', (req,res) => {
 });
 
 app.get('/send-booth-email', (req,res) => {
-  let email = req.query.email;
+  let email = decodeURIComponent(req.query.email);
+  console.log('Running booth email ', email);
+
+  let variation = optimizely.activate('EMAIL_EXPERIMENT', email);
+  console.log('Variation ', variation);
 
   let sender = 'Optimizely <me@' + domain +'>';
+
   let data = {
     from: sender,
     to: email,
-    subject: 'Thanks for stopping by Attic and Button!',
-    html: '<html><div align="center" style="max-width:580px; margin:0 auto;"><a href="http://www.atticandbutton.us/#userid=' + email + '"><img style="width:100%; margin:0 auto;" src="https://s3-us-west-2.amazonaws.com/optimizely-email-images/shirts.jpg"></a></div></html>'
+    subject: 'Thanks for stopping by Optimizely\'s Attic and Button!',
+    html: '<html><div align="center" style="max-width:580px; margin:0 auto;"><a href="http://localhost:3000/opticon-redirect?email=' + encodeURIComponent(req.query.email) + '"><img style="width:100%; margin:0 auto;" src="https://s3-us-west-2.amazonaws.com/optimizely-email-images/shirts.jpg"></a></div></html>'
   }
+  
+  //Check if user is part of a variation
+  if (variation === 'DEFAULT') {
+    //Sending email for original version
+    mailer.messages().send(data, (err, body) => {
+      console.log(body ? body : err);
+      return body ? res.sendStatus(200) : res.sendStatus(500);
+    });
+  } else if (variation === 'VARIATION_ONE') {
+    data.subject = "How do you like this Optimizely Fullstack experiment?"
+    //Sending email for Variation
+    mailer.messages().send(data, (err, body) => {
+      console.log(body ? body : err);
+      return body ? res.sendStatus(200) : res.sendStatus(500);
+    });
+  }
+});
 
-  mailer.messages().send(data, (err, body) => {
-    console.log(body ? body : err);
-    return body ? res.sendStatus(200) : res.sendStatus(500);
-  });
+app.get('/opticon-redirect', (req, res) => {
+  let email = req.query.email;
+  console.log('running redirect for ', email);
 
+  optimizely.track('EMAIL_OPENED', email);
+  console.log('Tracked Optly event');
+
+  res.redirect('http://www.atticandbutton.us/#userid=' + email);
 });
 
 app.get('/image-redirect', (req, res) => {
@@ -98,12 +145,7 @@ app.get('/image-redirect', (req, res) => {
   res.end(response.location);
 
 });
-//var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-
-//httpServer.listen(8080);
-//httpsServer.listen(8443);
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Running! GO CHECK LOCALHOST:3000");
+  console.log("Up and running - check localhost:3000 for local dev");
 });
